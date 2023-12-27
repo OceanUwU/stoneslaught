cefcbed24a0e6ed31801dce442637405c785e268
Ocean <ocean@ocean.lol>
2023-12-27 01:36:28 +0000

    draw death screen box

--- source/death.lua
+++ source/death.lua
@@ -2,22 +2,30 @@ class("Death").extends()
 
 function Death:init()
     self.sprite = playdate.graphics.sprite.new(playdate.graphics.image.new(1,1))
-    self.sprite:moveTo(400/2, -400)
+    self.sprite:moveTo(400/2, -240/2)
+    self.sprite:setZIndex(1)
     self.sprite:add()
-
 end
 
 function Death:open(score, highScore)
-    local img = playdate.graphics.image.new(playdate.graphics.image.new(200, 120))
+    local img = playdate.graphics.image.new(200, 120)
 	playdate.graphics.pushContext(img)
+    playdate.graphics.setColor(playdate.graphics.kColorBlack)
+    playdate.graphics.fillRect(0, 0, img.width, img.height)
+    playdate.graphics.setColor(playdate.graphics.kColorWhite)
+    playdate.graphics.fillRect(1, 1, img.width-1*2, img.height-1*2)
+    playdate.graphics.setColor(playdate.graphics.kColorBlack)
+    playdate.graphics.fillRect(3, 3, img.width-3*2, img.height-3*2)
+    playdate.graphics.setColor(playdate.graphics.kColorWhite)
+    playdate.graphics.fillRect(5, 5, img.width-5*2, img.height-5*2)
     local font<const> = playdate.graphics.getFont()
-    
 	playdate.graphics.popContext()
-    self:setImage(img)
+    self.sprite:setImage(img)
+    self.moveTimer = playdate.timer.new(800, self.sprite.y, 240/2, playdate.easingFunctions.outQuad)
 end
 
 function Death:close()
-    self.moveTimer = playdate.timer.new(800, self.sprite.y, -self.sprite.height/2,  playdate.easingFunctions.outQuad)
+    self.moveTimer = playdate.timer.new(800, self.sprite.y, -self.sprite.height/2, playdate.easingFunctions.outQuad)
 end
 
 function Death:update()
